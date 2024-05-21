98504db68c9bbbf8de4fc2558ff882d18ffb4367
mgawlik1 <01178275@pw.edu.pl>
2024-05-21 14:21:13 +0200

    adding output file

--- setup_cron.sh
+++ setup_cron.sh
@@ -3,6 +3,6 @@
 service cron start
 crontab -l > /tmp/mycron
 
-echo "*/11 * * * * root python3 /opt/app/scanner.py" >> /tmp/mycron
+echo "*/11 * * * * root python3 /opt/app/scanner.py >> /opt/output.txt" >> /tmp/mycron
 crontab /tmp/mycron
 rm /tmp/mycron
