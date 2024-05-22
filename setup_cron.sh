7b21a6b2d6919d2f11be70b176b30f02d3092cc2
mgawlik1 <01178275@pw.edu.pl>
2024-05-22 09:46:53 +0200

    cron modification without root

--- setup_cron.sh
+++ setup_cron.sh
@@ -3,6 +3,6 @@
 service cron start
 crontab -l > /tmp/mycron
 
-echo "*/11 * * * * root python3 /opt/app/scanner.py >> /var/log/scanner.log 2>&1" >> /tmp/mycron
+echo "*/11 * * * * python3 /opt/app/scanner.py >> /var/log/scanner.log 2>&1" >> /tmp/mycron
 crontab /tmp/mycron
 rm /tmp/mycron
