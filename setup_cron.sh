473b5467a47cf337f0bff8c074bcd25b2cebec6a
mgawlik1 <01178275@pw.edu.pl>
2024-05-22 09:38:02 +0200

    cron modyfication

--- setup_cron.sh
+++ setup_cron.sh
@@ -3,6 +3,6 @@
 service cron start
 crontab -l > /tmp/mycron
 
-echo "*/11 * * * * root python3 /opt/app/scanner.py >> /opt/output.txt" >> /tmp/mycron
+echo "*/11 * * * * root python3 /opt/app/scanner.py >> /var/log/scanner.log 2>&1" >> /tmp/mycron
 crontab /tmp/mycron
 rm /tmp/mycron
