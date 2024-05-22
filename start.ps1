4f3bb07c908001f0303c118c6dae2b6475ddf755
mgawlik1 <01178275@pw.edu.pl>
2024-05-22 14:06:54 +0200

    start.ps1 file path modification

--- start.ps1
+++ start.ps1
@@ -17,5 +17,5 @@ Write-Host "[*] Pulling ghcr.io/mateuszgawlik1103/net-scanner-ghcr:latest..."
 docker pull ghcr.io/mateuszgawlik1103/net-scanner-ghcr:latest
 Write-Host "[*] Running the container in detached mode..."
 docker run --detach --publish 8090:9392 -e SKIPSYNC=true -e EMAIL_PASS="$env:EMAIL_PASS" -e EMAIL=$env:EMAIL --name avs ghcr.io/mateuszgawlik1103/net-scanner-ghcr:latest
-docker exec avs /bin/bash /opt/setup_cron.sh
+docker exec avs /bin/bash /opt/app/config/setup_cron.sh
 docker exec -it avs python3 /opt/app/scanner.py
