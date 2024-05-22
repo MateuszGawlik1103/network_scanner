d46dd9b087955999857c8887298e0544e5915a4b
mgawlik1 <01178275@pw.edu.pl>
2024-05-22 11:13:52 +0200

    connection exception handler

--- src/scanner.py
+++ src/scanner.py
@@ -1,10 +1,10 @@
 from gvm.connections import TLSConnection
-from gvm.protocols.gmp import Gmp
+from gvm.protocols.latest import Gmp
 from gvm.transforms import EtreeTransform
 from gvm.xml import pretty_print
 from lxml import etree
 from gvm.protocols.gmpv208 import AlertCondition, AlertEvent, AlertMethod, ReportFormatType
-import defusedxml.ElementTree as ET
+from gvm.errors import GvmError
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -26,99 +26,110 @@ email = os.environ.get('EMAIL')
 # App password do maila
 email_pass = os.environ.get('EMAIL_PASS')
 
-def scan(target_name=None, hosts=None):
-    # Połączenie z gvmd przez socket Unix
-    connection = TLSConnection(hostname="localhost", port=9390)
-    transform = EtreeTransform()
-    # Utworzenie obiektu protokołu GMP
-    with Gmp(connection=connection, transform=transform) as gmp:
-         while True:
-            try:
-                gmp = Gmp(connection=connection)
-                authenticate(gmp)
-                break
-            except ConnectionRefusedError:
-                print("connection refused. Retrying after 30s...")
-                time.sleep(30)
-                continue
 
-    response = gmp.get_scanners()
-    # Pobranie informacji o skanerach
-    scanners_info = response.xpath('.//scanner')
-    # Pobranie nazw skanerów
-    scanner_names = [scanner.xpath('name/text()')[0] for scanner in scanners_info]
-    for scanner in scanners_info:
-        if scanner.xpath('name/text()')[0] == 'OpenVAS Default':
-            # Pobranie ID skanera OpenVAS Default
-            default_scanner_id = scanner.get('id')
-    print("Id of default Openvas scanner:")
-    print(default_scanner_id)
-
-
-    # Id konfiguracji 'Full and fast'
-    config_response = gmp.get_scan_configs()
-    # Pobranie informacji o konfiguracji skanowania
-    configs_info = config_response.xpath('.//config')
-    for config in configs_info:
-        if config.xpath('name/text()')[0] == 'Full and fast':
-            # Pobranie ID konfiguracji skanowania 'Full and fast'
-            config_id = config.get('id')
-
-
-
-    # Stworzenie celu
-    # Pobranie sieci z interfejsow
-    inet_adresses = host_disc.get_inet_addresses()
-    # Lista aktywnych hostow ze wszystkich interfejsow
-    all_active_hosts = []
-    for inet_adress in inet_adresses:
-        # Lista aktywnych hostow z poszczegolnych interfejsow
-        active_hosts = host_disc.get_active_hosts(inet_adress)
-        all_active_hosts.extend(active_hosts)
-    target_id = target_create.create_target("New target1", all_active_hosts)
-    
-    # Stworzenie polecenia (task)
-    response_task = gmp.create_task('Task1', config_id, target_id, default_scanner_id)
-    task_id = response_task.get('id')
-    print("Task id: ", task_id)
-    pretty_print(response_task)
-
-    # Start skanu
-    gmp.start_task(task_id=task_id)
-    task_ready = False
-    # Sprawdzenie, czy skan gotowy
-    while not task_ready:
-        got_task = gmp.get_task(task_id)
-        status = got_task.find(".//status").text
-        if status == 'Done':
-            task_ready = True
-        now_time = datetime.now()
-        print(f'{now_time.strftime("%H:%M:%S")}: {status}')
-        time.sleep(30)
-    ready_task = gmp.get_task(task_id)
-    report_id = ready_task.find(".//task").find('.//report').attrib.get("id")
-    report = gmp.get_report(report_id=report_id, details=True, report_format_id=ReportFormatType.PDF,
-                            filter_string="apply_overrides=0 levels=hmlg rows=100 min_qod=70 first=1 sort-reverse=severity")
-    report_element = report.find("report")
-    content = report_element.find("report_format").tail
-    binary_base64_encoded_pdf = content.encode('ascii')
-    binary_pdf = b64decode(binary_base64_encoded_pdf)
-    pdf_path = Path('report.pdf').expanduser()
-    # Zapis do pdf
-    pdf_path.write_bytes(binary_pdf)
-    delete_task()
-    delete_target()
-    send_email(email, email_pass)
-    print("PDF report created")
-
+def try_to_connect():
+    while True:
+        try:
+            connection = TLSConnection(hostname="localhost",port=9390)
+            print("Connection established...")
+            return connection
+        except GvmError | ConnectionRefusedError:
+            print("Connection failed. Retrying after 30s...")
+            time.sleep(30)
 
 def authenticate(gmp):
-    response = ET.fromstring(gmp.authenticate("admin", "admin"))
+    response = gmp.authenticate("admin", "admin")
     if response.get('status') != '200':
         print("Authentication failed")
         return False
     print("Authenticated to GVM")
     return True
+
+def scan(target_name=None, hosts=None):
+    # Połączenie z gvmd przez socket Unix
+    connection = try_to_connect()
+    transform = EtreeTransform()
+    # Utworzenie obiektu protokołu GMP
+    
+    while True:
+        try:
+            with Gmp(connection=connection, transform=transform) as gmp:  
+                authenticate(gmp)
+                response = gmp.get_scanners()
+                # Pobranie informacji o skanerach
+                scanners_info = response.xpath('.//scanner')
+                # Pobranie nazw skanerów
+                scanner_names = [scanner.xpath('name/text()')[0] for scanner in scanners_info]
+                for scanner in scanners_info:
+                    if scanner.xpath('name/text()')[0] == 'OpenVAS Default':
+                        # Pobranie ID skanera OpenVAS Default
+                        default_scanner_id = scanner.get('id')
+                print("Id of default Openvas scanner:")
+                print(default_scanner_id)
+
+
+                # Id konfiguracji 'Full and fast'
+                config_response = gmp.get_scan_configs()
+                # Pobranie informacji o konfiguracji skanowania
+                configs_info = config_response.xpath('.//config')
+                for config in configs_info:
+                    if config.xpath('name/text()')[0] == 'Full and fast':
+                        # Pobranie ID konfiguracji skanowania 'Full and fast'
+                        config_id = config.get('id')
+
+
+
+                # Stworzenie celu
+                # Pobranie sieci z interfejsow
+                inet_adresses = host_disc.get_inet_addresses()
+                # Lista aktywnych hostow ze wszystkich interfejsow
+                all_active_hosts = []
+                for inet_adress in inet_adresses:
+                    # Lista aktywnych hostow z poszczegolnych interfejsow
+                    active_hosts = host_disc.get_active_hosts(inet_adress)
+                    all_active_hosts.extend(active_hosts)
+                target_id = target_create.create_target("New target1", all_active_hosts)
+                
+                # Stworzenie polecenia (task)
+                response_task = gmp.create_task('Task1', config_id, target_id, default_scanner_id)
+                task_id = response_task.get('id')
+                print("Task id: ", task_id)
+                pretty_print(response_task)
+
+                # Start skanu
+                gmp.start_task(task_id=task_id)
+                task_ready = False
+                # Sprawdzenie, czy skan gotowy
+                while not task_ready:
+                    got_task = gmp.get_task(task_id)
+                    status = got_task.find(".//status").text
+                    if status == 'Done':
+                        task_ready = True
+                    now_time = datetime.now()
+                    print(f'{now_time.strftime("%H:%M:%S")}: {status}')
+                    time.sleep(30)
+                ready_task = gmp.get_task(task_id)
+                report_id = ready_task.find(".//task").find('.//report').attrib.get("id")
+                report = gmp.get_report(report_id=report_id, details=True, report_format_id=ReportFormatType.PDF,
+                                        filter_string="apply_overrides=0 levels=hmlg rows=100 min_qod=70 first=1 sort-reverse=severity")
+                report_element = report.find("report")
+                content = report_element.find("report_format").tail
+                binary_base64_encoded_pdf = content.encode('ascii')
+                binary_pdf = b64decode(binary_base64_encoded_pdf)
+                pdf_path = Path('report.pdf').expanduser()
+                # Zapis do pdf
+                pdf_path.write_bytes(binary_pdf)
+                delete_task()
+                delete_target()
+                print("PDF report created")
+                send_email(email, email_pass)
+                break
+        except ConnectionRefusedError:
+            print("connection refused. Retrying after 30s...")
+            time.sleep(30)
+            continue
+
+    
 
 
 
