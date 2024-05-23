1a4adc482af615d9c7307737658d6f902a9660d1
mgawlik1 <01178275@pw.edu.pl>
2024-05-23 23:46:10 +0200

    log correction

--- src/target_create.py
+++ src/target_create.py
@@ -18,7 +18,7 @@ def create_target(target_name, hosts):
         # Utworzenie celu
         response = gmp.create_target(name=target_name, hosts=hosts, port_range="80-443")
         target_id = response.get('id')
-        log_obj,log(f"Utworzono nowy cel o ID: {target_id}", lvl.INFO)
+        log_obj.log(f"Utworzono nowy cel o ID: {target_id}", lvl.INFO)
         return target_id
     
 #print(create_target("target1", ["192.160.0.1"]))
