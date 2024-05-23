6d7a93c9bc8a1db61b4a6e42d483bfa219d0caad
mgawlik1 <01178275@pw.edu.pl>
2024-05-23 22:46:46 +0200

    Interval change in cron

--- config/setup_cron.sh
+++ config/setup_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 service cron start
 crontab -l > /tmp/mycron
-echo "* * * * * /usr/bin/python3 /opt/app/scanner.py" >> /tmp/mycron
+echo "11 * * * * /usr/bin/python3 /opt/app/scanner.py" >> /tmp/mycron
 crontab /tmp/mycron
 rm /tmp/mycron
