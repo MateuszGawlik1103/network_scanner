35601127e79b334d99e8826f23e5be6318f7286a
mgawlik1 <01178275@pw.edu.pl>
2024-05-25 21:24:07 +0200

    small cron change for test 2

--- config/setup_cron.sh
+++ config/setup_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 service cron start
 crontab -l > /tmp/mycron
-echo "*/11 * * * * . $HOME/.profile; /usr/bin/python3 /opt/app/send.py" >> /tmp/mycron
+echo "*/11 * * * * . $HOME/.bashrc; /usr/bin/python3 /opt/app/send.py" >> /tmp/mycron
 crontab /tmp/mycron
 rm /tmp/mycron
